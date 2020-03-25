e4d304c6a424697d15b70fac464dbd6906c7f9d1
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-03-26 02:42:42 +0900

    add: ユーザー登録機能 + ログイン、ログアウト機能
    add: ユーザー登録完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def image_set(user)
+    user.image? ? user.image.url : 'default.png'
+  end
 end
---
+++ db/migrate/20200325173431_add_column_to_user.rb
@@ -0,0 +1,6 @@
+class AddColumnToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string, null: false, default: ''
+    add_column :users, :admin, :boolean, null: false, default: false
+  end
+end
