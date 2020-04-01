e5011d7228bc2f796f0c28bdc91b685450385323
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-04-01 16:39:13 +0900

    add: 体重・体脂肪の詳細機能

--- app/controllers/body_statuses_controller.rb
+++ app/controllers/body_statuses_controller.rb
@@ -1,14 +1,21 @@
 class BodyStatusesController < ApplicationController
+  before_action :set_body_status, only: [:show]
+
   def index
     @body_statuses = BodyStatus.where(user_id: current_user.id).order(created_at: :desc)
   end
 
-  def show
-  end
+  def show;end
 
   def new
   end
 
   def edit
+  end
+
+  private
+
+  def set_body_status
+    @body_status = BodyStatus.find(params[:id])
   end
 end
