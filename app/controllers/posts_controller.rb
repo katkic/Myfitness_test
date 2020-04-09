2a923499d40208ebe23d11f959ac7f4bfefc9f69
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-04-10 01:29:38 +0900

    add: 投稿作成機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,25 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to posts_path, notice: '投稿を保存しました'
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
