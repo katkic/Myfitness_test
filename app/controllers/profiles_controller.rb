d087492ba60671bbecf144130001950272589580
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-04-13 19:52:43 +0900

    add: ユーザープロフィールの詳細表示機能

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,25 @@
 class ProfilesController < ApplicationController
+  before_action :set_user, only: :show
+
   def index
     @users = User.all
   end
 
   def show
+    @post_count = @user.posts.count
+    @following_count = @user.following.count
+    @followers_count = @user.followers.count
   end
 
   def new
   end
 
   def edit
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
