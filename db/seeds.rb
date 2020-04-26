d69af7643dfdef4423a8d847db929e662f9b6653
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-04-27 00:49:43 +0900

    add: ユーザーのシードデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: 'taro', email: 'taro@test.com', password: 'kawaiitaro')
+User.create!(name: 'jiro', email: 'jiro@test.com', password: 'kawaiijiro')
+User.create!(name: 'saburo', email: 'saburo@test.com', password: 'kawaiiwaburo')
+User.create!(name: 'shiro', email: 'shiro@test.com', password: 'kawaiishiro')
+User.create!(name: 'goro', email: 'goro@test.com', password: 'kawaiigoro')
+
+Profile.create!(user_id: 3)
+Profile.create!(user_id: 4)
+Profile.create!(user_id: 5)
+Profile.create!(user_id: 6)
+Profile.create!(user_id: 7)
+
+User.create!(name: 'rokuro', email: 'rokuro@test.com', password: 'kawaiirokuro')
+Profile.create!(user_id: 8, self_introduction: 'こんにちは、六郎です。よろしくお願いします')
+
+
