7c464f5ba2fe518f2ebadcfdcb64bc92a04c7ab4
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-04-29 01:11:43 +0900

    update: 不要なコードを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,24 +7,20 @@ module ApplicationHelper
     profile.header_image && profile.header_image.url ? profile.header_image.url : 'header_image_default.png'
   end
 
-  def set_post_picture(post)
-    post.picture && post.picture.url ? post.picture.url : 'post_picture_default.png'
-  end
-
   def set_exercise_icon(exercise)
     exercise.icon && exercise.icon.url ? exercise.icon.url(:thumb) : 'exercise_icon.png'
   end
 
   def set_body_picture1(body_status)
-    body_status.picture1 && body_status.picture1.url ? body_status.picture1.url(:thumb) : 'no_image_gray_medium.png'
+    body_status.picture1 && body_status.picture1.url ? body_status.picture1.url : 'no_image_gray_medium.png'
   end
 
   def set_body_picture2(body_status)
-    body_status.picture2 && body_status.picture2.url ? body_status.picture2.url(:thumb) : 'no_image_gray_medium.png'
+    body_status.picture2 && body_status.picture2.url ? body_status.picture2.url : 'no_image_gray_medium.png'
   end
 
   def set_body_picture3(body_status)
-    body_status.picture3 && body_status.picture3.url ? body_status.picture3.url(:thumb) : 'no_image_gray_medium.png'
+    body_status.picture3 && body_status.picture3.url ? body_status.picture3.url : 'no_image_gray_medium.png'
   end
 
   def format_date_time(date)
