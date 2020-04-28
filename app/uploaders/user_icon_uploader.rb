cb80e02d5f7e7771409038c933903ed3c5da16eb
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-04-28 23:35:09 +0900

    add: 画像のプレビュー機能と削除機能

--- app/uploaders/user_icon_uploader.rb
+++ app/uploaders/user_icon_uploader.rb
@@ -1,10 +1,11 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class UserIconUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
+  process :resize_to_limit => [100, 100]
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -30,14 +31,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process resize_to_fit: [50, 50]
+  #   process resize_to_fit: [100, 100]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
