3e3511b960f702b4c5cd86df7a29d610a357ef91
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-04-30 00:32:02 +0900

    update: 画像表示 + 画像プレビューメソッドのリファクタリング  + css調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,30 +1,6 @@
 module ApplicationHelper
-  def set_icon(user)
-    user.icon && user.icon.url ? user.icon.url : 'default.png'
-  end
-
-  def set_header_image(profile)
-    profile.header_image && profile.header_image.url ? profile.header_image.url : 'header_image_default.png'
-  end
-
-  def set_post_picture(post)
-    post.picture && post.picture.url ? post.picture.url : 'no_image_gray_medium.png'
-  end
-
-  def set_exercise_icon(exercise)
-    exercise.icon && exercise.icon.url ? exercise.icon.url(:thumb) : 'exercise_icon.png'
-  end
-
-  def set_body_picture1(body_status)
-    body_status.picture1 && body_status.picture1.url ? body_status.picture1.url : 'no_image_gray_medium.png'
-  end
-
-  def set_body_picture2(body_status)
-    body_status.picture2 && body_status.picture2.url ? body_status.picture2.url : 'no_image_gray_medium.png'
-  end
-
-  def set_body_picture3(body_status)
-    body_status.picture3 && body_status.picture3.url ? body_status.picture3.url : 'no_image_gray_medium.png'
+  def set_image(image_obj, image_name)
+    image_obj.present? ? image_obj.url : image_name
   end
 
   def format_date_time(date)
